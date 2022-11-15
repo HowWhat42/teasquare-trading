6cd1c0c452465f9bbba829a87c935f0a6dfd2c3c
HowWhat42 <charlau30@gmail.com>
2022-11-15 14:36:28 +0100

    fix: trade checking accounts

--- src/controllers/onTrade.ts
+++ src/controllers/onTrade.ts
@@ -1,65 +1,69 @@
 import { accounts, checkBalance, setLeverage } from "./account"
 import { prisma } from '../config'
+import ccxt from 'ccxt'
 
 type side = "buy" | "sell"
+const defaultAccount = new ccxt.bybit()
 
 export const openTrade = async (rawPair: string, side: side, tradeLeverage: number) => {
-    const symbol = rawPair.split('USDT')[0].split('BUSD')[0]
-    const marketIndex = `${symbol}/USDT:USDT`
-    const markets = await accounts[0].loadMarkets()
-    const market = markets[marketIndex]
-    if (market) {
-        const limit = Number(market?.limits?.amount?.min)
-        const pair = `${symbol}USDT`
-        accounts.map(async (account) => {
-            try {
-                const credentials = await prisma.credentials.findMany({ where: { api: account.apiKey } })
-                const openTrades = await prisma.trades.findMany({
-                    where: {
-                        credentialId: Number(credentials[0].id),
-                        pair,
-                        open: true
+    if (accounts.length > 0) {
+        const symbol = rawPair.split('USDT')[0].split('BUSD')[0]
+        const marketIndex = `${symbol}/USDT:USDT`
+        const markets = await defaultAccount.loadMarkets()
+        const market = markets[marketIndex]
+        if (market) {
+            const limit = Number(market?.limits?.amount?.min)
+            const pair = `${symbol}USDT`
+            accounts.map(async (account) => {
+                try {
+                    const credentials = await prisma.credentials.findMany({ where: { api: account.apiKey } })
+                    const openTrades = await prisma.trades.findMany({
+                        where: {
+                            credentialId: Number(credentials[0].id),
+                            pair,
+                            open: true
+                        }
+                    })
+                    if (openTrades.length > 0) {
+                        console.log('Trade already open')
+                        return
                     }
-                })
-                if (openTrades.length > 0) {
-                    console.log('Trade already open')
-                    return
-                }
-                const usdtBalance = await checkBalance(account, 'USDT')
-                const bankrollSize = credentials[0].bankrollPercentage / 100
-                const price = await accounts[0].fetchTicker(pair)
-                const quantity = Number((usdtBalance.free * bankrollSize / Number(price.last)).toFixed(3))
-                if (quantity <= limit) {
-                    console.log('USDT is not enough to open trade')
-                    return
-                }
-                console.log('USDT is enough to open trade')
-                let leverage = tradeLeverage
-                if (tradeLeverage > credentials[0].maxLeverage) {
-                    console.log('Leverage is too high')
-                    leverage = credentials[0].maxLeverage
-                }
-                await setLeverage(account, pair, leverage)
+                    const usdtBalance = await checkBalance(account, 'USDT')
+                    const bankrollSize = credentials[0].bankrollPercentage / 100
+                    const price = await defaultAccount.fetchTicker(pair)
+                    const quantity = Number((usdtBalance.free * bankrollSize / Number(price.last)).toFixed(3))
+                    if (quantity <= limit) {
+                        console.log('USDT is not enough to open trade')
+                        return
+                    }
+                    console.log('USDT is enough to open trade')
+                    let leverage = tradeLeverage
+                    if (tradeLeverage > credentials[0].maxLeverage) {
+                        console.log('Leverage is too high')
+                        leverage = credentials[0].maxLeverage
+                    }
+                    await setLeverage(account, pair, leverage)
 
-                const trade = await account.createMarketOrder(pair, side, quantity)
-                await prisma.trades.create({
-                    data: {
-                        pair,
-                        leverage,
-                        size: trade.amount,
-                        entryPrice: Number(price.last),
-                        side,
-                        credentialId: credentials[0].id
-                    }
-                })
-                console.log('trade open')
-            } catch (error) {
-                throw error
-            }
-        })
-    } else {
-        console.log(`No market for ${rawPair}`)
-        return
+                    const trade = await account.createMarketOrder(pair, side, quantity)
+                    await prisma.trades.create({
+                        data: {
+                            pair,
+                            leverage,
+                            size: trade.amount,
+                            entryPrice: Number(price.last),
+                            side,
+                            credentialId: credentials[0].id
+                        }
+                    })
+                    console.log('trade open')
+                } catch (error) {
+                    throw error
+                }
+            })
+        } else {
+            console.log(`No market for ${rawPair}`)
+            return
+        }
     }
 }
 
@@ -77,7 +81,7 @@ export const closeTrade = async (rawPair: string) => {
                 }
             })
             if (openTrades.length > 0) {
-                const price = await accounts[0].fetchTicker(pair)
+                const price = await defaultAccount.fetchTicker(pair)
                 const openTrade = openTrades[0]
                 const side = openTrade.side === 'buy' ? 'sell' : 'buy'
                 await account.createMarketOrder(pair, side, openTrade.size, undefined, { reduce_only: true })
